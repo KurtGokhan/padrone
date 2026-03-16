27cecfd84927c84f0ef469427028bbb0045e3f5d
Gokhan Kurt <krtgkn@gmail.com>
2026-03-16 14:44:15 +0300

    fix:reduce number of exports

--- packages/padrone/src/index.ts
+++ packages/padrone/src/index.ts
@@ -1,47 +1,16 @@
-export type { PadroneFieldMeta as PadroneArgsMeta } from './args.ts';
 export { asyncSchema, buildReplCompleter, createPadrone } from './create.ts';
-export type {
-  HelpArgumentInfo as HelpOptionInfo,
-  HelpFormat,
-  HelpInfo,
-  HelpPositionalInfo as HelpArgumentInfo,
-  HelpSubcommandInfo,
-} from './formatter.ts';
-export type { HelpPreferences } from './help.ts';
-export type { InteractiveMode, InteractivePromptConfig, PadroneRuntime, ResolvedPadroneRuntime } from './runtime.ts';
-export type {
-  InferArgsInput,
-  InferArgsOutput,
-  InferCommand,
-  InferConfigInput,
-  InferConfigOutput,
-  InferEnvInput,
-  InferEnvOutput,
-} from './type-helpers.ts';
-export type { HasInteractive, IsAsyncSchema, MaybePromise, OrAsync, OrAsyncMeta } from './type-utils.ts';
+export type { HelpInfo } from './formatter.ts';
+export type { InteractiveMode, InteractivePromptConfig, PadroneRuntime } from './runtime.ts';
+export type { InferArgsInput, InferArgsOutput, InferCommand } from './type-helpers.ts';
 export type {
   AnyPadroneCommand,
   AnyPadroneProgram,
-  AsyncPadroneSchema,
   PadroneActionContext,
   PadroneBuilder,
-  PadroneCliPreferences,
   PadroneCommand,
-  PadroneCommandConfig,
   PadroneCommandResult,
-  PadroneEvalPreferences,
   PadroneParseResult,
   PadronePlugin,
   PadroneProgram,
-  PadroneReplPreferences,
-  PadroneReplSpacing,
   PadroneSchema,
-  PluginBaseContext,
-  PluginExecuteContext,
-  PluginExecuteResult,
-  PluginParseContext,
-  PluginParseResult,
-  PluginValidateContext,
-  PluginValidateResult,
 } from './types.ts';
-export type { WrapConfig, WrapResult } from './wrap.ts';
